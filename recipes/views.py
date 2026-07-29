67bf22d4db147bb814d634fb6c014f79c3bd825a
Altamirando-ti <altamirando.ti@gmail.com>
2026-07-29 11:23:21 -0300

    Criação da página contato na pasta templates, função render na views e criação de outra pasta recipes dentro da pasta templates como boa prática para evitar confusão de caminhos pelo sistema

--- recipes/views.py
+++ recipes/views.py
@@ -3,14 +3,11 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-# def home(request):
-#     return HttpResponse('Página home')
-
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'recipes/home.html') #namespace
 
 def sobre(request):
     return HttpResponse('Página sobre')
 
 def contato(request):
-    return HttpResponse('Página contato')
+    return render(request, 'recipes/contato.html')
