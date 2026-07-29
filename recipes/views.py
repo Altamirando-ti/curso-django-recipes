541c594ad23a2414e1c1542b6560557fd8954509
Altamirando-ti <altamirando.ti@gmail.com>
2026-07-29 11:07:24 -0300

    Criação da pasta templates para as páginas html e utilização da função render para a página home

--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+# def home(request):
+#     return HttpResponse('Página home')
+
+def home(request):
+    return render(request, 'home.html')
+
+def sobre(request):
+    return HttpResponse('Página sobre')
+
+def contato(request):
+    return HttpResponse('Página contato')
