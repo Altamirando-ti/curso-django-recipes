cdd792ce942f8f50250e216acf6c865c9fd05906
Altamirando-ti <altamirando.ti@gmail.com>
2026-09-07 00:23:28 -0300

    Finalizando video-aula 29

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'recipes/home.html') #namespace
-
-def sobre(request):
-    return HttpResponse('Página sobre')
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
+    return render(request, 'recipes/pages/home.html') #namespace
